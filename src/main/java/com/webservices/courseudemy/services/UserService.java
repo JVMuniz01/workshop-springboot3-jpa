a83cb89f52afad331250c3f84c3b4854223da134
Muniz <j040vmr@gmail.com>
2025-01-05 15:53:44 -0300

    Component registration

---
+++ src/main/java/com/webservices/courseudemy/services/UserService.java
@@ -0,0 +1,27 @@
+package com.webservices.courseudemy.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.webservices.courseudemy.entities.User;
+import com.webservices.courseudemy.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User>findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id){
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+	
+}
